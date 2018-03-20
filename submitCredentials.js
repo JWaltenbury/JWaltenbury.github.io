f3d1db5bd96871edc8abd7682aa56d21384d47cf
JWaltenbury <36353626+JWaltenbury@users.noreply.github.com>
2018-03-19 22:33:02 -0400

    Add files via upload
    ICE revision

--- submitCredentials.js
+++ submitCredentials.js
@@ -14,13 +14,13 @@ function submitCredentials(){
     var userPassword = document.getElementById("userPassword").value;
     var displayString = "";
 
-    if(userName == "Thom232" && userPassword == "password")
+    if(userName == "Ted426" && userPassword == "password")
     {
         window.location = "informationPage.html"; //Reference: https://stackoverflow.com/questions/6321000/how-can-i-go-to-a-new-page-from-within-javascript
     }  
     else
     {
-        displayString = "Invalid Username or Password. Please try again. (Thom232, password)";
+        displayString = "Invalid Username or Password. Please try again. (Ted426, password)";
         document.getElementById("errorMessage").innerHTML = displayString; 
     }   
 }
