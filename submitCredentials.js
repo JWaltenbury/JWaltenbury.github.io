ac2dd04d26c4c1b2abd3a0c532d63b73b466d6b9
JWaltenbury <36353626+JWaltenbury@users.noreply.github.com>
2018-04-08 16:57:44 -0400

    Add files via upload

--- submitCredentials.js
+++ submitCredentials.js
@@ -25,6 +25,22 @@ function submitCredentials(){
     }   
 }
 
+function submitCredentials2(){
+    var userName = document.getElementById("userName").value;
+    var userPassword = document.getElementById("userPassword").value;
+    var displayString = "";
+
+    if(userName == "Ted426" && userPassword == "password")
+    {
+        window.location = "account.html"; //Reference: https://stackoverflow.com/questions/6321000/how-can-i-go-to-a-new-page-from-within-javascript
+    }  
+    else
+    {
+        displayString = "Invalid Username or Password. Please try again. (Ted426, password)";
+        document.getElementById("errorMessage").innerHTML = displayString; 
+    }   
+}
+
 function resetCredentials(){
     document.getElementById("userName").value = "";
     document.getElementById("userPassword").value = "";  
